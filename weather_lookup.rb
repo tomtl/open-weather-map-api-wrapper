89dc89e9b05d90dcc4cb3e1b669f95e4ab3451f8
Tom Lee <tom_t_lee@yahoo.com>
2015-08-19 17:10:08 +0000

    Refactor into objects

--- weather_lookup.rb
+++ weather_lookup.rb
@@ -5,18 +5,33 @@ require "pry"
 class LocationNotFound < StandardError; end
 class RequestFailed < StandardError; end
 
-def get_temp(location)
-  url = "http://api.openweathermap.org/data/2.5/weather"
-  units = "imperial"
+class OpenWeatherMapClient 
+  def initialize(http_client = Faraday.new)
+    @http_client = http_client
+  end
   
-  http_client = Faraday.new
-  response = http_client.get(url, q: location, units: units)
-  data = JSON.load(response.body)
+  def current_temperature(location)
+    url = "http://api.openweathermap.org/data/2.5/weather"
+    units = "imperial"
+    data = make_request(url, q: location, units: units)
 
-  raise LocationNotFound.new(data["message"]) unless data["main"]
-  data["main"]["temp"] if data
-rescue Faraday::Error => e
-  raise RequestFailed, e.message, e.backtrace
+    raise LocationNotFound.new(data["message"]) unless data["main"]
+    data["main"]["temp"]
+  end
+  
+  private
+  
+  def make_request(url, params={})
+    response = @http_client.get(url, params)
+    JSON.load(response.body)
+  rescue Faraday::Error => e
+    raise RequestFailed, e.message, e.backtrace
+  end
+end
+
+def get_temp(location)
+  openweathermap_client = OpenWeatherMapClient.new
+  openweathermap_client.current_temperature(location)
 end
 
 if $0 == __FILE__
